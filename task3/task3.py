467fdba95250b14d05e9a01fa22b60f8424b5287
kai <kiger1228@163.com>
2017-09-05 01:19:36 +0800

    add --open file and get file name--

--- task3/task3.py
+++ task3/task3.py
@@ -2,12 +2,13 @@
 # -*- coding: UTF-8 -*-
 
 from Tkinter import *
+import tkFileDialog
 
 top = Tk()
 top.geometry("450x250")
 
-welcome = Label(top, text="注册界面")  # 注册标签
-welcome.pack()
+f5 = Button(top, text="刷新设备")  # 注册标签
+f5.pack(pady=5)
 
 frm = Frame(top)  # 界面框架
 frm_L = Frame(frm)
@@ -18,24 +19,29 @@ frm_L.pack(side=LEFT)
 frm_R.pack(side=RIGHT)
 frm2.pack()
 
-label_1 = Label(frm_L, text="设备1",width=10,height=2)  # 用户名标签
-label_2 = Label(frm_L, text="设备2",width=10,height=2)  # 密码标签
-label_3 = Label(frm_L, text="设备3",width=10,height=2)  # 再次输入密码标签
-label_1.pack()
-label_2.pack()
-label_3.pack()
+label_1 = Label(frm_L, text="设备1",width=8,height=1,)  # 用户名标签
+label_2 = Label(frm_L, text="设备2",width=8,height=1)  # 密码标签
+label_3 = Label(frm_L, text="设备3",width=8,height=1)  # 再次输入密码标签
+label_1.pack(pady=20)
+label_2.pack(pady=20)
+label_3.pack(pady=20)
 
 
-button_1 = Button (frm_R, text="安装",width=10,height=2)  # 输入框-用户名
-button_1.pack()
+def getFile():
+    filename = str(tkFileDialog.askopenfilename(initialdir ='/'))
+    tishi_1=Toplevel()
+    text_filename=Text(tishi_1,text=filename)
+    text_filename.pack()
+
+button_1 = Button (frm_R, text="安装",width=8,height=1,command=getFile)  # 输入框-用户名
+button_1.pack(pady=16)
+button_2 = Button (frm_R, text="安装",width=8,height=1)
+button_2.pack(pady=18)
+button_3 = Button (frm_R, text="安装",width=8,height=1)
+button_3.pack(pady=18)
 
 
-button_2 = Button (frm_R, text="安装",width=10,height=2)
-button_2.pack()
 
-
-button_3 = Button (frm_R, text="安装",width=10,height=2)
-button_3.pack()
 
 
 
