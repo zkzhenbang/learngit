94ddaaab6ea2f562a46bd78f371e1ad8ac817f3e
kai <kiger1228@163.com>
2017-09-10 23:31:38 +0800

    change task3

--- task3/task3.py
+++ task3/task3.py
@@ -8,95 +8,68 @@ import os
 import re
 
 
- # 获得adb devices命令下设备信息
-devices = str(os.popen("adb devices"))
-
-# 获得设备 serial number
-serial_nos = []
-for item in devices.split():
-    filters = ['list', 'of', 'device', 'devices', 'attached']
-    if item.lower() not in filters:
-        serial_nos.append(item)
-
-# 点击安装按钮调用方法
-def getFile():
-    # filename = tkFileDialog.askopenfilename(initialdir ='/')
-    p = os.popen("adb devices")
-    a = p.read()
-    deviceInfo = subprocess.check_output('adb devices -l')
-    deviceName = re.findall(r'device product:(.*)\smodel', deviceInfo, re.S)[0]
-    print a
-
-    # sysInfo = subprocess.check_output('adb shell cat /system/build.prop')
-    # androidVersion = re.findall("version.release=(\d\.\d)*", sysInfo, re.S)[0]
-    # deviceInfo = subprocess.check_output('adb devices -l')
-    # deviceName = re.findall(r'device product:(.*)\smodel', deviceInfo, re.S)[0]
-    # print filename
-    # print serial_nos
-    # print androidVersion
-    # print deviceName
-
-
-def connectDevcie():
-    '''''检查设备是否连接成功，如果成功返回True，否则返回False'''
-    try:
-        '''''获取设备列表信息，并用"\r\n"拆分'''
-        deviceInfo = subprocess.check_output('adb devices').split("\r\n")
-        '''''如果没有链接设备或者设备读取失败，第二个元素为空'''
-        if deviceInfo[1] == '':
-            return False
-        else:
-            return True
-    except Exception, e:
-        print "Device Connect Fail:", e
-
-
-def getAndroidVersion():
-    try:
-        if connectDevcie():
-            # 获取系统设备系统信息
-            sysInfo = subprocess.check_output('adb shell cat /system/build.prop')
-            # 获取安卓版本号
-            androidVersion = re.findall("version.release=(\d\.\d)*", sysInfo, re.S)[0]
-            return androidVersion
-        else:
-            return "Connect Fail,Please reconnect Device..."
-    except Exception, e:
-        print "Get Android Version:", e
-
-
-def getDeviceName():
-    try:
-        if connectDevcie():
-            # 获取设备名
-            deviceInfo = subprocess.check_output('adb devices -l')
-            deviceName = re.findall(r'device product:(.*)\smodel', deviceInfo, re.S)[0]
-            return deviceName
-        else:
-            return "Connect Fail,Please reconnect Device..."
-    except Exception, e:
-        print "Get Device Name:", e
 
 
 
+
+
+
+# 创建窗体
 top = Tk()
-top.geometry("450x250")
+top.title("app install")
+top.geometry("380x250+740+380")
+top.resizable(0, 0)      #禁止缩放
 
-f5 = Button(top, text="刷新设备")  # 注册标签
+# 左侧Label显示的内容，更改内容可以修改show_f5.set(XXXX)
+show_f5 = StringVar()
+show_f5.set('欢迎使用')
+
+# 按下刷新设备按钮
+def pressF5():
+    showf5 = os.popen("adb devices")
+    a=showf5.read()
+    show_f5.set(a)
+
+# 按下按钮1安装调用方法
+def pressButton_1():
+    filename1 = tkFileDialog.askopenfilename(initialdir ='/')
+    # 向serial number设备安装filename的包
+    # install_1=os.popen("adb -s <serial number> install filename1")
+
+#  按下按钮2安装调用方法
+def pressButton_2():
+    filename2 = tkFileDialog.askopenfilename(initialdir ='/')
+    # 向serial number设备安装filename2的包
+    # install_1=os.popen("adb -s <serial number> install filename2")
+
+#  按下按钮3安装调用方法
+def pressButton_3():
+    filename1 = tkFileDialog.askopenfilename(initialdir ='/')
+    # 向serial number设备安装filename的包
+    # install_1=os.popen("adb -s <serial number> install filename3")
+
+show_l = Frame(top)
+show_l.pack(side=LEFT)
+show_r = Frame(top)
+show_r.pack(side=RIGHT)
+
+messgeShow = Label(show_l, textvariable=show_f5)
+messgeShow.pack()
+messgeShow.propagate(0)
+
+f5 = Button(show_r, text="刷新设备",command=pressF5)
 f5.pack(pady=5)
 
-frm = Frame(top)  # 界面框架
+frm = Frame(show_r)
 frm_L = Frame(frm)
 frm_R = Frame(frm)
-frm2 = Frame(top)
 frm.pack()
 frm_L.pack(side=LEFT)
 frm_R.pack(side=RIGHT)
-frm2.pack()
 
-label_1 = Label(frm_L, text="设备1",width=8,height=1,)  # 用户名标签
-label_2 = Label(frm_L, text="设备2",width=8,height=1)  # 密码标签
-label_3 = Label(frm_L, text="设备3",width=8,height=1)  # 再次输入密码标签
+label_1 = Label(frm_L, text="设备1",width=8,height=1)
+label_2 = Label(frm_L, text="设备2",width=8,height=1)
+label_3 = Label(frm_L, text="设备3",width=8,height=1)
 label_1.pack(pady=20)
 label_2.pack(pady=20)
 label_3.pack(pady=20)
@@ -104,12 +77,12 @@ label_3.pack(pady=20)
 
 
 
-button_1 = Button (frm_R, text="安装",width=8,height=1,command=getFile)  # 输入框-用户名
-button_1.pack(pady=16)
-button_2 = Button (frm_R, text="安装",width=8,height=1)
-button_2.pack(pady=18)
-button_3 = Button (frm_R, text="安装",width=8,height=1)
-button_3.pack(pady=18)
+button_1 = Button (frm_R, text="安装",width=8,height=1,command=pressButton_1)
+button_1.pack(pady=16,padx=10)
+button_2 = Button (frm_R, text="安装",width=8,height=1,command=pressButton_2)
+button_2.pack(pady=18,padx=10)
+button_3 = Button (frm_R, text="安装",width=8,height=1,command=pressButton_3)
+button_3.pack(pady=18,padx=10)
 
 
 
